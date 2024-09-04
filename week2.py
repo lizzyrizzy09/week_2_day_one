cf869bb8c4021aee9c2693c9704611f794d31442
Gonzalez Alcala <egonzalezal@cps.k12.il.us>
2024-09-04 09:29:49 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,9 @@
+# ask someone what day it is 
+# day = input("What day is it?")
+# print("Today is" + day)
+# # ask them for their favorite holiday 
+holiday =input("What is your favorite holiday?")
+print("Your favorite holdiday is" + holiday)
+# print out the day and their favorite holiday
+print("Today is" + day )
+# a sentence 
